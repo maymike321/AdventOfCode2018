9d93521c767e6ca97a3fd329ab6972d5c37f65bb
MikeMay321 <mmay@dealersocket.com>
2018-12-06 11:51:24 -0700

    make day2 part2 solution less bad

--- Day2/Part2/solution.hs
+++ Day2/Part2/solution.hs
@@ -1,16 +1,16 @@
-compareBoxes :: (String, String) -> String
-compareBoxes ((x:xs), (y:ys))
-        | x == y = [x] ++ compareBoxes (xs, ys)
-        | otherwise = compareBoxes (xs, ys)
-compareBoxes ([], _) = []
-compareBoxes (_, []) = []
+compareBoxes :: String -> String -> String
+compareBoxes (x:xs) (y:ys)
+        | x == y = [x] ++ compareBoxes xs ys
+        | otherwise = compareBoxes xs ys
+compareBoxes [] _ = []
+compareBoxes _ [] = []
 
 f :: [String] -> String
 f (x:xs)
         | length matchingBoxes > 0 = head matchingBoxes
         | otherwise = f xs
         where   matchingBoxes = filter (\box -> length box == boxLength - 1) comparedBoxes
-                comparedBoxes = map (\box -> compareBoxes (x, box)) xs
+                comparedBoxes = map (compareBoxes x) xs
                 boxLength = (length x)
 f [] = ""
 
